de1cdfb9308fe463219103bd42b7fe318a391bfc
Sylvain <syl@thinkpad.localhost>
2024-09-02 18:11:59 +0200

    Fix helpers 2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,3 +4,9 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
+myynh_set_permissions () {
+	chown -R $app: "$install_dir"
+	chmod 750 "$install_dir"
+	chmod -R o-rwx "$install_dir"
+	chmod -R +x "$install_dir/bin/*"
+}
