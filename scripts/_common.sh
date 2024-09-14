55699a8a380286eade32800db8323a10da8aa737
Sylvain <syl@thinkpad.localhost>
2024-09-14 08:43:00 +0200

    Fix permissions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,10 @@
 
 myynh_set_permissions () {
 	chown -R $app: "$install_dir"
-	chmod 750 "$install_dir"
+	chmod u=rwX,g=rX,o= "$install_dir"
 	chmod -R o-rwx "$install_dir"
 	chmod -R +x "$install_dir/bin/"
+
+	chown $app:root "/var/log/$app"
+	chmod -R u=rwX,g=rX,o= "/var/log/$app"
 }
